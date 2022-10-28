f71bc9766253aaec5b57ed5274cf6856b003d8d7
Julian Rivera <julianarevircoins@gmail.com>
2022-10-28 00:23:35 -0400

    BST

--- index.js
+++ index.js
@@ -85,3 +85,54 @@ function productSum(array, arrCounter = 1) {
 
   return result * arrCounter;
 }
+// Binary SearcH Tree
+
+// recursion
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+  return findClosestValueInBstHelper(tree, target, tree.value);
+}
+
+function findClosestValueInBstHelper(tree, target, closest) {
+  if (tree === null) return closest;
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+    closest = tree.value;
+  }
+  if (target < tree.value) {
+    return findClosestValueInBstHelper(tree.left, target, closest);
+  } else if (target > tree.value) {
+    return findClosestValueInBstHelper(tree.right, target, closest);
+  } else {
+    return closest;
+  }
+}
+// This is the class of the input tree. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+//
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+  return findClosestValueInBstHelper(tree, target, tree.value);
+}
+function findClosestValueInBstHelper(tree, target, closest) {
+  let currentNode = tree;
+  while (currentNode !== null) {
+    if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+      closest = currentNode.value;
+    }
+    if (target < currentNode.value) {
+      currentNode = currentNode.left;
+    } else if (target > currentNode.value) {
+      currentNode = currentNode.right;
+    } else {
+      break;
+    }
+  }
+  return closest;
+}
